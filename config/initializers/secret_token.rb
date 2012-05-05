47605e36c4b2626041a8e2639b1764f9c8d7b80c
Owen Moony <owenm@boxdice.com.au>
2012-05-06 09:19:19 +1000

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickStart::Application.config.secret_token = '413035364f8bee76bd1732224aa5fdc6a148ce7594c6c9ad4c911b661bdd2535d0b7f94a65655d3d8e85dc1cfc093bf56dacac73c483b6938f766b138c1b2e0d'
